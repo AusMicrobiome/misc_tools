53473713e4ed95f86d06cfc37efa783c370db76f
Andrew Bissett <Andrew.Bissett@csiro.au>
2022-04-07 15:30:25 +1000

    add traits column to give expected input/output

--- reformat_AM_3col.py
+++ reformat_AM_3col.py
@@ -10,7 +10,7 @@ A script to convert long (3 col + taxonomy) format table as exported by AM datab
 (ASV'z X Samples (rows X cols) table.
 
 Input is the *.csv table downloaded from the AM denoised dataportal https://data.bioplatforms.com/bpa/otu/.
-This must have column names ['OTU','Amplicon','Kingdom','Phylum','Class','Order','Family','Genus','Species'] and be
+This must have column names ['OTU','Amplicon','Kingdom','Phylum','Class','Order','Family','Genus','Species', 'Traits'] and be
 csv.
 
 Output is a rectangular table and includes the taxonomy contained in the downloaded 3 column table.
@@ -19,7 +19,7 @@ Output is a rectangular table and includes the taxonomy contained in the downloa
 
 infile=input("path to input file: ")
 output=input("name of your output file: ")
-expected_cols=['Sample ID','OTU','OTU Count','Amplicon','Kingdom','Phylum','Class','Order','Family','Genus','Species']
+expected_cols=['Sample ID','OTU','OTU Count','Amplicon','Kingdom','Phylum','Class','Order','Family','Genus','Species', 'Traits']
 import pandas as pd
 #read the long table in
 
@@ -39,7 +39,7 @@ if tableW.values.sum() == tableL['OTU Count'].sum(): #check the table values
     print("adding taxonomy")
 
     #add the taxonomy back in
-    taxonomy = ['OTU','Amplicon','Kingdom','Phylum','Class','Order','Family','Genus','Species']
+    taxonomy = ['OTU','Amplicon','Kingdom','Phylum','Class','Order','Family','Genus','Species', 'Traits']
     taxL = tableL[taxonomy] #get the taxonomy data
     taxL1 = taxL.drop_duplicates(keep='first').set_index('OTU') #remove the duplicates
     merge=pd.merge(tableW,taxL1,left_index=True, right_index=True, how='inner') #merge the OTU abundance and taxonomies
@@ -47,7 +47,7 @@ if tableW.values.sum() == tableL['OTU Count'].sum(): #check the table values
 else:
     print("error: abundance of input table != abundance of pivoted table")
 
-if merge.drop(['Amplicon','Kingdom','Phylum','Class','Order','Family','Genus','Species'], axis=1).values.sum() ==  tableL['OTU Count'].sum():
+if merge.drop(['Amplicon','Kingdom','Phylum','Class','Order','Family','Genus','Species', 'Traits'], axis=1).values.sum() ==  tableL['OTU Count'].sum():
     merge.to_csv(output+'.csv') #write the table out
     print(".....finished!")
 else:
